d26cab018f983fd3710c58149c43046037734760
Changzhou Li <45784976+Changzhouli3309@users.noreply.github.com>
2021-03-28 19:40:36 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  // const [pokemons, setPokemos] = useState([]);
+  const [todos, setTodos] = useState([]);
+
+  useEffect(function () {
+    const getList = async () => {
+      // const resp = await fetch("https://pokeapi.co/api/v2/pokemon?limit=100");
+      // const obj = await resp.json();
+      // const pokeList = obj.results;
+      // console.log(pokeList)
+      // setPokemos(pokeList)
+
+      const resp = await fetch("https://jsonplaceholder.typicode.com/todos");
+      const todos = await resp.json();
+      console.log(todos);
+      setTodos(todos);
+    }
+    getList()
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        123
       </header>
-    </div>
+    </>
   );
 }
 
